bea143ded5186558c9207b4338f10137c73285bf
MaedaKouta <maedakoutaa@gmail.com>
2022-02-25 17:44:58 +0900

    Initial Commit

---
+++ MiniApp18-NavigationDateTransfer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiniApp18-NavigationDateTransfer
+//
+//  Created by 前田航汰 on 2022/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
